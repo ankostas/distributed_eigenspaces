7ced0a04d9147a87e5465f3a0a192c2ffed4e3d5
TimeEscaper <rentgeny05@gmail.com>
2019-12-18 15:21:17 +0300

    Communication fixed

--- distributed_eigenspace/distributed.py
+++ distributed_eigenspace/distributed.py
@@ -8,8 +8,10 @@ import numpy as np
 
 class Node:
     def __init__(self, broker_host):
+        # TODO: Implement proper credentials management
         self.connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host=broker_host))
+            pika.ConnectionParameters(host=broker_host,
+                                      credentials=pika.PlainCredentials("distrib", "test")))
         self.channel = self.connection.channel()
         self.channel.queue_declare(queue='master')
         self.channel.queue_declare(queue='slaves')
@@ -18,26 +20,29 @@ class Node:
 class SlaveNode(Node):
     def __init__(self, broker_host):
         super().__init__(broker_host)
+        print("Slave Start listening")
         self.channel.basic_consume(queue='slaves', on_message_callback=self.callback_)
 
     def start(self):
         self.channel.start_consuming()
 
     def callback_(self, channel, method, properties, body):
-        print("Slave: Received")
         request = json.loads(body)
+        print("Slave: Received, batchid: " + str(request["batchId"]))
+        print(request["batch"])
         eigenspace = self.compute_eigenspace_(request["batch"], request["rank"])
         response = dict()
         response["batchId"] = request["batchId"]
-        response["eigenspace"] = eigenspace.to_list()
+        response["eigenspace"] = eigenspace.tolist()
         self.send_to_master_(str(json.dumps(response)))
         channel.basic_ack(delivery_tag=method.delivery_tag)
 
     def send_to_master_(self, message):
+        print("Sending to Master")
         self.channel.basic_publish(exchange='', routing_key='master', body=message)
 
     def compute_eigenspace_(self, batch, rank):
-        sigma = np.zeros((batch[0].shape[0], batch[0].shape[1]))
+        sigma = np.zeros((len(batch[0]), len(batch[0])))
         for x in batch:
             vec = np.array(x)
             sigma += vec @ vec.T
@@ -49,6 +54,7 @@ class SlaveNode(Node):
 class MasterNode(Node):
     def __init__(self, broker_host):
         super().__init__(broker_host)
+        print("Master Start listening")
         self.channel.basic_consume(queue='master', on_message_callback=self.callback_)
 
     def start(self):
@@ -58,21 +64,21 @@ class MasterNode(Node):
         test_data["batch"] = list()
         for i in range(0, 10):
             test_data["batch"].append(np.random.rand(5, 1).tolist())
-        self.send_to_slave_(str(json.dumps(test_data)))
+        self.send_to_slaves_(str(json.dumps(test_data)))
 
         self.channel.start_consuming()
 
     def callback_(self, channel, method, properties, body):
-        print("Master: received")
         request = json.loads(body)
+        print("Master: Received, batchid: " + str(request["batchId"]))
         batch_id = request["batchId"]
         eigenspace = np.array(request["eigenspace"])
-        print(batch_id)
         print(eigenspace)
         channel.basic_ack(delivery_tag=method.delivery_tag)
 
-    def send_to_slave_(self, message):
-        self.channel.basic_publish(exchange='', routing_key='slave', body=message)
+    def send_to_slaves_(self, message):
+        print("Sending to slaves: ")
+        self.channel.basic_publish(exchange='', routing_key='slaves', body=message)
 
 
 def run_master(broker):
